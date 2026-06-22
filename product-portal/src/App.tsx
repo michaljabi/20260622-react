7b278e2de380a8a79bf2dc7a093874eac8156694
Jabi <michal.jabi@gmail.com>
2026-06-22 14:47:42 +0200

    task(7): Install and configure `tailwind` in project.

--- product-portal/src/App.tsx
+++ product-portal/src/App.tsx
@@ -1,27 +1,36 @@
 // import heroImg from "./assets/hero.png";
-import { Sample } from "./common/Sample.tsx";
+// import { Sample } from "./common/Sample.tsx";
 // import { Sample as SampleAlias } from "./common/Sample.tsx";
 
 function App() {
   console.log(3 + 90 + 2 * 4);
 
-  const dynamicJSX = <span>Hello</span>;
+  // const dynamicJSX = <span>Hello</span>;
+
+  return (
+    <div className="min-h-full">
+        <nav className="bg-indigo-400 p-2"> MenuBar </nav>
+        <main className="mx-auto max-w-5xl px-4 py-8">
+            <div> Strona... </div>
+        </main>
+    </div>
+  )
 
   // console.log - NIE W JSX!
-  return (
-    <>
-      {/* <div style="background-color: red"> tak się nie da w JSX */}
-      <div>
-        <Sample />
-        {/* <SampleAlias /> */}
-        <span id={"myelement_" + 1}>Hello</span>
-        {dynamicJSX}
-        {<span id={"myelement_" + 2}>Hello</span>}
-      </div>
-      {/* <img src={heroImg} /> */}
-    </>
-    // <div></div>
-  );
+  // return (
+  //   <>
+  //     {/* <div style="background-color: red"> tak się nie da w JSX */}
+  //     <div>
+  //       <Sample />
+  //       {/* <SampleAlias /> */}
+  //       <span id={"myelement_" + 1}>Hello</span>
+  //       {dynamicJSX}
+  //       {<span id={"myelement_" + 2}>Hello</span>}
+  //     </div>
+  //     {/* <img src={heroImg} /> */}
+  //   </>
+  //   // <div></div>
+  // );
 }
 
 export default App;
