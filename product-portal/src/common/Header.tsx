43b8afe19a239f6a3fc5823e6d3ccf611293fffc
Jabi <michal.jabi@gmail.com>
2026-06-23 10:52:23 +0200

    task(9): instalacja ikon w projekcie.

--- product-portal/src/common/Header.tsx
+++ product-portal/src/common/Header.tsx
@@ -1,3 +1,12 @@
+/*
+Trick, żeby pozbyć się komunikatów o błędach jeśli nie użyjemy Zap!
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line
+*/
+import { Zap } from 'lucide-react'
+
 // Dobra praktyka w TS to pokazanie na górze komponentu jego wartości
 // WEJŚCOWYCH czyli props
 type HeaderProps = {
@@ -5,12 +14,17 @@ type HeaderProps = {
     itemsCount?: number;
 }
 
+
 export function Header({ title = 'Product Portal', itemsCount }: HeaderProps) {
   return (
     <header className="mx-auto max-w-6xl flex items-center p-3 md:p-6 border-slate-600 border-b">
        {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator */} 
        {/* https://developer.mozilla.org/en-US/docs/Glossary/Falsy */} 
-      <span className="md:text-xl lg:text-2xl mr-6">{title}{itemsCount ? '(' + itemsCount + ')' : '' }</span>
+   
+      <div className="mr-3 bg-indigo-300 text-black p-1 px-2 rounded">
+        <Zap />
+      </div>
+       <span className="md:text-xl lg:text-2xl mr-6">{title}{itemsCount ? '(' + itemsCount + ')' : '' }</span>
     </header>
   );
 }
