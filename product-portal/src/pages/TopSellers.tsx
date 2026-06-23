42dbd80a8c45139a117959f11d16d86dd6f46627
Jabi <michal.jabi@gmail.com>
2026-06-23 15:25:11 +0200

    task(17): /top-sellers page.

---
+++ product-portal/src/pages/TopSellers.tsx
@@ -0,0 +1,12 @@
+export function TopSellers() {
+  return (
+    <section>
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold">Top Sellers</h1>
+        <p className="text-sm text-zinc-500">
+          Check out our most successful sellers
+        </p>
+      </div>
+    </section>
+  );
+}
--- product-portal/src/router.tsx
+++ product-portal/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { Products } from "./pages/Products.tsx";
+import { TopSellers } from "./pages/TopSellers.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -8,8 +9,9 @@ export const router = createBrowserRouter([
     Component: App,
     children: [
       { index: true, Component: Products },
-    //   { path: "hello", Component: () => "HELLO" },
-    //   { path: "hello", Component: () => "HELLO2" },
+      { path: "top-sellers", Component: TopSellers },
+      //   { path: "hello", Component: () => "HELLO" },
+      //   { path: "hello", Component: () => "HELLO2" },
       { path: "*", Component: () => <div>Page not found :( </div> },
     ],
   },
